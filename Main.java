1b5e8ac4b02a54452b497368c9a75da23bd6fa50
su leo <157701661+sulingxi@users.noreply.github.com>
2025-01-30 20:42:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,27 @@
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Main extends JFrame {
     Mappa mp = null;
+    static Semaphore s1=new Semaphore(1);
+    static Semaphore s2=new Semaphore(0);;
+    public static ArrayList<Integer> X = new ArrayList<>();
+    public static ArrayList<Integer> O = new ArrayList<>();
 
     public static void main(String[] args) {
         Main mainFrame = new Main();
-        Giocattore giocattore = new Giocattore("paolo", mainFrame.mp);
-        giocattore.initializeNumbers();
-
-        // Inizializzazione dell'array
-        System.out.println("Inizializzazione di un array:");
-        for (int i = 0; i < 9; i++) {
-            System.out.print(giocattore.numbers[i] + " ");
-        }
-        System.out.println();
-
-        // Creazione e avvio dei thread
-        Thread thread = new Thread(giocattore, "Thread-1");
-        Thread thread1 = new Thread(giocattore, "Thread-2");
-        thread.start();
-        thread1.start();
+        Giocattore giocattore1 = new Giocattore("paolo", mainFrame.mp,s1,s2,"X");
+        Giocattore giocattore2 = new Giocattore("paolo", mainFrame.mp,s2,s1,"O");
+        giocattore1.start();
+        giocattore2.start();
     }
 
     public Main() {
-        mp = new Mappa(); // Creazione di un oggetto pannello personalizzato
-        this.add(mp);     // Aggiunta del pannello personalizzato alla finestra
-
-        this.setSize(900, 900); // Imposta la dimensione della finestra (larghezza 900, altezza 900)
-        this.setVisible(true);  // Imposta la finestra come visibile
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Imposta l'operazione di chiusura
+        mp = new Mappa();
+        this.add(mp);
+        this.setSize(900, 900);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
